da5c014c3c3c71ba9cbf1274b5fafe91679a6133
TTNguyen <nttriet9a4@gmail.com>
2019-02-15 17:47:01 +0700

    add firebase

--- app/src/main/java/com/example/trietnguyen/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/trietnguyen/kotlinmessenger/MainActivity.kt
@@ -4,26 +4,47 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         sigup_button_register.setOnClickListener {
-            val email =  email_editText_register.text.toString()
+
+            val email = email_editText_register.text.toString()
             val password = password_editText_register.text.toString()
+            Log.d("main", "get here" + email)
 
-            Log.d("MainActivity", "email " + email)
-            Log.d("MainActivity", "password " + password)
-        }
+            auth = FirebaseAuth.getInstance()
 
-        already_have_account_textView.setOnClickListener {
-            Log.d("MainActivity", "Clicked")
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("main", "createUserWithEmail:success")
+                        val user = auth.currentUser
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("main", "createUserWithEmail:failure", task.exception)
+
+
+                    }
+
+                    // ...
+                }
+
+            already_have_account_textView.setOnClickListener {
+                Log.d("MainActivity", "Clicked")
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
