41101697270806fee9674a17b5d3862dabf69a68
TTNguyen <nttriet9a4@gmail.com>
2019-02-15 17:03:02 +0700

    signup + login screen

--- app/src/main/java/com/example/trietnguyen/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/trietnguyen/kotlinmessenger/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.trietnguyen.kotlinmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sigup_button_register.setOnClickListener {
+            val email =  email_editText_register.text.toString()
+            val password = password_editText_register.text.toString()
+
+            Log.d("MainActivity", "email " + email)
+            Log.d("MainActivity", "password " + password)
+        }
+
+        already_have_account_textView.setOnClickListener {
+            Log.d("MainActivity", "Clicked")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
